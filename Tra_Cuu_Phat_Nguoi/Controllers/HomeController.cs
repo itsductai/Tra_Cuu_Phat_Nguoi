5f026d646a54216bf97d6cec3bc41b8bd4df48b0
Nguyen Duc Tai <ductaaii.2002@gmail.com>
2025-01-11 10:45:23 +0700

    Add project files.

---
+++ Tra_Cuu_Phat_Nguoi/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tra_Cuu_Phat_Nguoi.Models;
+
+namespace Tra_Cuu_Phat_Nguoi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Tracuu(string ten)
+        {
+            // Trả về biển số xe đã nhập cho yêu cầu AJAX
+            return Content(ten);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Tra_Cuu_Phat_Nguoi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tra_Cuu_Phat_Nguoi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
